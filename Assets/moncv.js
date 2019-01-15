0f51801e214f61f9368dc58ff31c877fbe7fea7b
MarineCns <marine.caucanas@gmail.com>
2019-01-15 17:50:40 +0100

    modif ok

---
+++ Assets/moncv.js
@@ -0,0 +1,11 @@
+﻿// JavaScript source code
+function displayshow(div) {
+    document.getElementById('formations').style.display = "none";
+    document.getElementById('pro').style.display = "none";
+    document.getElementById('perso').style.display = "none";
+    document.getElementById('contact').style.display = "none";
+    document.getElementById(div).style.display = "block";
+}
+$('.nav-item').click(function () {
+    $('#navbarNav').removeClass('show');
+});
--- moncv.js
+++
@@ -1,8 +0,0 @@
-﻿// JavaScript source code
-function displayshow(div) {
-    document.getElementById('formations').style.display = "none"; 
-    document.getElementById('pro').style.display = "none"; 
-    document.getElementById('perso').style.display = "none"; 
-    document.getElementById('contact').style.display = "none";
-    document.getElementById(div).style.display = "block"; 
-}
